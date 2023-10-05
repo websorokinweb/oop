3dde6886b96adb3b5fc22540cb471cc4aa5c84e9
websorokinweb <btrubiuk@gmail.com>
2023-10-05 11:16:47 +0200

    Add files via upload

---
+++ first/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpisz bok a: ");
+            string newAString = Console.ReadLine();
+            Console.Write("Wpisz bok b: ");
+            string newBString = Console.ReadLine();
+            Console.Write("Wpisz bok c: ");
+            string newCString = Console.ReadLine();
+            double newA = double.Parse(newAString);
+            double newB = double.Parse(newBString);
+            double newC = double.Parse(newCString);
+
+            Triangle triangle = new Triangle(newA, newB, newC);
+            if(triangle.isReal() )
+            {
+                Console.WriteLine(triangle.getHerone());
+            }
+        }
+    }
+}
