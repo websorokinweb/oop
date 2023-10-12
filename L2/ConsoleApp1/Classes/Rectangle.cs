c46c79789c3211aecf6cceebbdcb7d1e71123fb8
websorokinweb <btrubiuk@gmail.com>
2023-10-12 11:14:05 +0200

    Add files via upload

---
+++ L2/ConsoleApp1/Classes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    internal class Rectangle
+    {
+        public double width;
+        public double height;
+
+        public Rectangle(double width, double height) {
+            if(width <= 0 || height <= 0)
+            {
+                Console.WriteLine("Boki prostokątu muszą być dodatne");
+                throw new ArgumentException("Wypełnij prawidlowo długość boków");
+            }
+            else
+            {
+                this.width = width;
+                this.height= height;
+            }
+        }
+
+        public double CalculateArea()
+        {
+            return width * height;
+        }
+    }
+}
